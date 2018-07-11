fb0b21b6aff3b67c2de18fe9108697d39b9441c4
Colin Arenz <colinarenz@allenai.org>
2018-07-11 15:01:38 -0700

    Fix initial SHA256 hash.

--- beaker.rb
+++ beaker.rb
@@ -3,7 +3,7 @@ class Beaker < Formula
   homepage "https://beaker.allenai.org"
   url "https://github.com/allenai/beaker/releases/download/v20180630/beaker_mac.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
   version "v20180630"
-  sha256 "2684feb90ea1320e68cd795ae597f8db0db17667"
+  sha256 "68e292a9ed85f20b716543c6702868137c242d17818b63b9efda593014ee48bf"
 
   def install
     bin.install "beaker"
