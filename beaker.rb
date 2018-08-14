62c996eb63b5fc9916cd537561cc7067c94770bf
Colin Arenz <colinarenz@allenai.org>
2018-08-13 17:30:30 -0700

    Change CLI repository.

--- beaker.rb
+++ beaker.rb
@@ -1,9 +1,9 @@
 class Beaker < Formula
   desc "Beaker command-line tool."
   homepage "https://beaker.allenai.org"
-  url "https://github.com/allenai/beaker/releases/download/v20180712/beaker_mac.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
-  version "20180712"
-  sha256 "b8320e89a20c2563f22e55b57167c5bcfdae383922c77fedf116ca5b3d3c4765"
+  url "https://github.com/allenai/beaker/releases/download/v20180723/beaker_mac.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
+  version "20180723"
+  sha256 "5abb5f32ebfa08d02e9d3702063107c30aa72e0574d427cf35dfdadcfa9749ed"
 
   def install
     bin.install "beaker"
