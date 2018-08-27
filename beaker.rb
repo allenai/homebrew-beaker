cbf203b77b30a5c225a8c63064a0e4b539dd0d54
Colin Arenz <colin.arenz@gmail.com>
2018-08-27 11:57:09 -0700

    Remove PrivateRepo download strategy.

--- beaker.rb
+++ beaker.rb
@@ -1,7 +1,7 @@
 class Beaker < Formula
   desc "Beaker command-line tool."
   homepage "https://beaker.allenai.org"
-  url "https://github.com/allenai/beaker/releases/download/v20180820/beaker_mac.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
+  url "https://github.com/allenai/beaker/releases/download/v20180820/beaker_mac.tar.gz"
   version "20180820"
   sha256 "c576c4b8e9bdefec8f756ba87075c75568e599234f94f187a0d73e705d5ddf46"
 
