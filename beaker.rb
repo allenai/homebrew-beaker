e5d5931990a1cd4097cd1338641220c44d6d0f67
goreleaserbot <goreleaser@carlosbecker.com>
2018-10-18 12:50:48 -0700

    Brew formula update for beaker version v20181018

--- beaker.rb
+++ beaker.rb
@@ -1,9 +1,9 @@
 class Beaker < Formula
   desc "Beaker command-line tool."
   homepage "https://beaker.allenai.org"
-  url "https://github.com/allenai/beaker/releases/download/v20180820/beaker_mac.tar.gz"
-  version "20180820"
-  sha256 "c576c4b8e9bdefec8f756ba87075c75568e599234f94f187a0d73e705d5ddf46"
+  url "https://github.com/allenai/beaker/releases/download/v20181018/beaker_mac.tar.gz"
+  version "20181018"
+  sha256 "cf0c87860d28bf4bfc0b24c7017a80d3c44f00b59468c0567f9c87219e51170d"
 
   def install
     bin.install "beaker"
