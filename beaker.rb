0c77ac264076c3f3ec69f50cf88cc32ec20dec42
beakerbot <43896404+beakerbot@users.noreply.github.com>
2020-10-20 14:59:25 -0700

    Brew formula update for beaker version v20201020

--- beaker.rb
+++ beaker.rb
@@ -1,9 +1,9 @@
 class Beaker < Formula
   desc "Beaker command-line tool."
   homepage "https://beaker.allenai.org"
-  url "https://github.com/allenai/beaker/releases/download/v20201015/beaker_mac.tar.gz"
-  version "20201015"
-  sha256 "b82b7cd0f7a6bb65ae0c469f07e9fb27f8568de55432230b7a35f6b87648294e"
+  url "https://github.com/allenai/beaker/releases/download/v20201020/beaker_mac.tar.gz"
+  version "20201020"
+  sha256 "212a401d7933930d4acce8f96591c648b86e401e6248ce469b07c38621c73894"
 
   def install
     bin.install "beaker"
