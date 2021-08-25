2a0422a1b486061345babeccf7dd09a86645eea8
beakerbot <43896404+beakerbot@users.noreply.github.com>
2021-08-25 16:29:21 -0700

    Brew formula update for beaker version v1.3.6

--- beaker.rb
+++ beaker.rb
@@ -1,9 +1,9 @@
 class Beaker < Formula
   desc "Beaker command-line tool."
   homepage "https://beaker.allenai.org"
-  url "https://github.com/allenai/beaker/releases/download/v1.3.5/beaker_mac.tar.gz"
-  version "1.3.5"
-  sha256 "d95a8d84516116cf9e9b5c62ba585eef33a199ec7dbd4cded305a94ebf9cbda4"
+  url "https://github.com/allenai/beaker/releases/download/v1.3.6/beaker_mac.tar.gz"
+  version "1.3.6"
+  sha256 "92dc11b25b5343c97358695f739b329540bac2e5954bdb1ad692eaea73f4081e"
 
   def install
     bin.install "beaker"
